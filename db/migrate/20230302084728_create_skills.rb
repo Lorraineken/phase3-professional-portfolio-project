7e78632ea5322b8b114978deecd879b6e0cd9e09
Lorraineken <lorrainekupa@gmail.com>
2023-03-02 11:52:36 +0300

    created users,projects and skills tables

--- db/migrate/20230302084728_create_skills.rb
+++ db/migrate/20230302084728_create_skills.rb
@@ -1,4 +1,10 @@
 class CreateSkills < ActiveRecord::Migration[6.1]
   def change
+    create_table :skills do |t|
+      t.string :name, null:false
+      t.string :category
+      t.integer :project_id
+      t.integer :user_id
+    end
   end
 end
